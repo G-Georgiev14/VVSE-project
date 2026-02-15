f79e7ab31e5aaea8296146a457b7bd8b33e6d3c5
sanguineblade <leo.aquarius.corona@gmail.com>
2026-02-15 14:42:33 +0200

    added-sessions-to-the-database

---
+++ backend_database/database.py
@@ -0,0 +1,35 @@
+import os
+from sqlalchemy import create_engine, update
+from sqlalchemy.orm import sessionmaker
+from models import Base, RepoBase, CommitBase
+
+DATA_ROOT = "database"
+
+GLOBAL_DB_URL = f"sqlite:///{DATA_ROOT}/users.db"
+global_engine = create_engine(GLOBAL_DB_URL, connect_args={"check_same_thread": False})
+GlobalSessionLocal = sessionmaker(bind=global_engine)
+
+def init_global_database():
+    if not os.path.exists(DATA_ROOT):
+        os.makedirs(DATA_ROOT)
+    Base.metadata.create_all(bind=global_engine)
+
+def get_repo_metadata_session(username: str, repo_name: str):
+    path = os.path.join(DATA_ROOT, username, repo_name)
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+
+    database_file = os.path.join(path, "metadata.db")
+    engine = create_engine(f"sqlite:///{database_file}")
+    RepoBase.metadata.create_all(bind=engine)
+    return sessionmaker(bind=engine)()
+
+def get_commit_database_session(username: str, repo_name: str, commit_hash: str):
+    path = os.path.join(DATA_ROOT, username, repo_name)
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+
+    database_file = os.path.join(path, f"{commit_hash}.db")
+    engine = create_engine(f"sqlite:///{database_file}")
+    CommitBase.metadata.create_all(bind=engine)
+    return sessionmaker(bind=engine)()
