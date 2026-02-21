5a285dc1cda51f5cc50e6cd534e79059a152af29
sanguineblade <leo.aquarius.corona@gmail.com>
2026-02-21 17:25:30 +0200

    new-website-structure

--- backend_database/models.py
+++ backend_database/models.py
@@ -10,7 +10,7 @@ CommitBase = declarative_base()
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True,  autoincrement=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True)
     password = Column(String)
@@ -20,14 +20,14 @@ class User(Base):
 class Repo(Base):
     __tablename__ = "repos"
     
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
     user_id = Column(Integer)
 
 class RepoMetadata(RepoBase):
     __tablename__ = "commit_history"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     commit_name = Column(String)
     commit_hash = Column(String, unique=True)
     message = Column(String)
