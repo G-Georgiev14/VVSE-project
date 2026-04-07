b956f714348a67a1e9b49d06d03a2f6185c0512b
sanguineblade <leo.aquarius.corona@gmail.com>
2026-04-07 19:11:08 +0300

    idk

--- gitbuild/src/main/java/com/sanguine/gitbuild/command/PingCommand.java
+++ gitbuild/src/main/java/com/sanguine/gitbuild/command/PingCommand.java
@@ -11,8 +11,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 public class PingCommand {
 
